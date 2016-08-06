57fa49c963bb11d12d6859fe1c1732bb3e6c3864
zhoukaimao <zhoukaimao@163.com>
2016-08-06 21:21:24 +0800

    new models

---
+++ Assets/Scripts/UI/HUD/CarStub.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarStub : MonoBehaviour {
+    float speed = 0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow)&&speed<100)
+        {
+            
+            speed++;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && speed > 0) 
+        { 
+            speed--; 
+        }
+
+	}
+    public float GetSpeed()
+    {
+        return speed;
+    }
+}
---
+++ Assets/Scripts/UI/HUD/HUDManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDManager : MonoBehaviour {
+    public UITexture pointer;
+    public UILabel speedLabel;
+    public CarStub car;
+    float speedHistory=0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        speedLabel.text = car.GetSpeed().ToString();
+        pointer.parent.transform.Rotate(Vector3.forward,(speedHistory-car.GetSpeed())*2.2f);
+        speedHistory = car.GetSpeed();
+        pointer.color = car.GetSpeed()/100*Color.red+(1-car.GetSpeed()/100)*Color.blue;
+        //pointer.transform.rotation = new Quaternion(0,0,car.GetSpeed(),0);
+        //pointer.transform.rotation.z = 10;
+	}
+}
